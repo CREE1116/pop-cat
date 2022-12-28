8190c37a1a16e24c4fe0135a3adfa1dcaff4c75a
leejongmin <60211689@mju.ac.kr>
2022-12-28 22:41:19 +0900

    모바일 환경 고려

--- src/components/Popcat.jsx
+++ src/components/Popcat.jsx
@@ -3,6 +3,12 @@ import sound from "../sound/pop-cat-original-meme_3ObdYkj.mp3";
 import PopCat from "../img/pop-cat.png";
 import unPopCat from "../img/unpop-cat.png";
 import styles from "./Popcat.module.css";
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
 function Popcat() {
   const [imageSRC, changeImage] = useState(unPopCat);
 
@@ -16,14 +22,22 @@ function Popcat() {
   };
   return (
     <div className={styles.container}>
-      <img
-        className={styles.popcat}
-        src={imageSRC}
-        onMouseDown={popDown}
-        onMouseUp={popUp}
-        width="400"
-        height="400"
-      ></img>
+      <BrowserView>
+        <img
+          className={styles.popcat}
+          src={imageSRC}
+          onMouseDown={popDown}
+          onMouseUp={popUp}
+        />
+      </BrowserView>
+      <MobileView>
+        <img
+          className={styles.popcat}
+          src={imageSRC}
+          onTouch={popDown}
+          onTouchEnd={popUp}
+        />
+      </MobileView>
     </div>
   );
 }
