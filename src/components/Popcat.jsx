933846827c28e4a156f83c02936dcf3d6e3de0e0
leejongmin <60211689@mju.ac.kr>
2022-12-29 10:11:44 +0900

    개발자도구 추가

--- src/components/Popcat.jsx
+++ src/components/Popcat.jsx
@@ -1,33 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import sound from "../sound/pop-cat-original-meme_3ObdYkj.mp3";
 import PopCat from "../img/pop-cat.png";
 import unPopCat from "../img/unpop-cat.png";
 import styles from "./Popcat.module.css";
+import { isMobile } from "react-device-detect";
 
 function Popcat(prop) {
   const audio = new Audio(sound);
   const [popImage, setPopImage] = useState(unPopCat);
   const [count, setCount] = useState(0);
-  const press = () => {
+  const press = (e) => {
+    e.stopPropagation();
+    console.log("press");
     setPopImage(PopCat);
     audio.play();
   };
-  const release = () => {
+  const release = (e) => {
+    e.stopPropagation();
+    console.log("release");
     setPopImage(unPopCat);
     setCount((prev) => prev + 1);
   };
+  function getConvertedEventType(type) {
+    if (isMobile) {
+      if (type === "mousedown") {
+        console.log("touchstart");
+        type = "touchstart";
+      } else if (type === "mouseup") {
+        console.log("touchend");
+        type = "touchend";
+      }
+    }
+
+    return type;
+  }
+  useEffect(() => {
+    document.addEventListener(
+      getConvertedEventType("mousedown"),
+      (e) => press(e),
+      false
+    );
+    document.addEventListener(
+      getConvertedEventType("mouseup"),
+      (e) => release(e),
+      false
+    );
+  }, []);
   return (
     <>
       <div className={styles.container}>
         <h1 className={styles.title}>POP CAT</h1>
         <h1 className={styles.counter}>{count}</h1>
-        <img
-          src={popImage}
-          alt="popcat"
-          className={styles.popcat}
-          onMouseDown={press}
-          onMouseUp={release}
-        />
+        <img src={popImage} alt="popcat" className={styles.popcat} />
       </div>
     </>
   );
