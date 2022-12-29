d768e9c198b65af35cba8638887d5782e52e0755
leejongmin <60211689@mju.ac.kr>
2022-12-29 09:42:49 +0900

    디자인

--- src/components/Popcat.jsx
+++ src/components/Popcat.jsx
@@ -3,27 +3,33 @@ import sound from "../sound/pop-cat-original-meme_3ObdYkj.mp3";
 import PopCat from "../img/pop-cat.png";
 import unPopCat from "../img/unpop-cat.png";
 import styles from "./Popcat.module.css";
-import { BrowserView, MobileView } from "react-device-detect";
+
 function Popcat(prop) {
   const audio = new Audio(sound);
   const [popImage, setPopImage] = useState(unPopCat);
+  const [count, setCount] = useState(0);
   const press = () => {
     setPopImage(PopCat);
     audio.play();
   };
   const release = () => {
     setPopImage(unPopCat);
+    setCount((prev) => prev + 1);
   };
   return (
-    <div className={styles.container}>
-      <img
-        src={popImage}
-        alt="popcat"
-        className={styles.popcat}
-        onMouseDown={press}
-        onMouseUp={release}
-      />
-    </div>
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.title}>POP CAT</h1>
+        <h1 className={styles.counter}>{count}</h1>
+        <img
+          src={popImage}
+          alt="popcat"
+          className={styles.popcat}
+          onMouseDown={press}
+          onMouseUp={release}
+        />
+      </div>
+    </>
   );
 }
 export default Popcat;
